8a08fc8b213938abf291be7dbeb28888f7f94b60
Edon Leite <edonleite@gmail.com>
2025-04-26 18:19:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('nav');
 
-    menuToggle.addEventListener('click', (EdonGomesLeite.pdf) => {
+    menuToggle.addEventListener('click', () => {
         navLinks.classList.toggle('active');
     });
 });
