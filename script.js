cd0c353386b103aaa03bd3faf8058037dd234eb7
Edon Leite <edonleite@gmail.com>
2025-04-26 17:21:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('nav');
 
-    menuToggle.addEventListener('click', (CV Edon Gomes Leite.pdf) => {
+    menuToggle.addEventListener('click', () => {
         navLinks.classList.toggle('active');
     });
 });
